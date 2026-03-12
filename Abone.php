1b45fa8e38780fa686ad408a05f7b05a0186214f
GeoTech-Dila <ozturkdila670@gmail.com>
2026-03-12 13:16:41 +0300

    hard reset sonrası

---
+++ Abone.php
@@ -0,0 +1,4 @@
+<?php
+echo "Lütfen beni takip etmeyi ve gönderilerimi beğenmeyi unutma";
+echo "Herkese Şimdiden iyi seyirler";
+echo "hard reset sonrası tekrar beğenmeyi ve takip etmeyi unutmayın";
